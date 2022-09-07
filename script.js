66b3e5c28a074c9a697db67f29e4c5168be8e725
Brooke Holley <106936368+boklenhle@users.noreply.github.com>
2022-09-07 17:20:36 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+var InputName = prompt(`What's your name?`)
+
+InputName;
+
+listenuserinput(click, doesthis)
+
+listenuserinput(enterbutton, doesthis)
+
+ 
+
+//text box with place holder "description" need submit button that recognizes listenuserinput for click and enter button and then clears the textbox
+
+//will add description and time stamp
+
+//needs to be a way to edit time stamp (for if you forget to add one)
+
+//needs to be a delete button for any added logs
