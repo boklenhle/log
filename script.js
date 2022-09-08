379bd51413f16958232a8ba6117976597b899fe5
Brooke Holley <brookeholley@live.com>
2022-09-08 16:50:34 -0400

    added event listener and notes

--- script.js
+++ script.js
@@ -2,12 +2,6 @@ var InputName = prompt(`What's your name?`)
 
 InputName;
 
-listenuserinput(click, doesthis)
-
-listenuserinput(enterbutton, doesthis)
-
- 
-
 //text box with place holder "description" need submit button that recognizes listenuserinput for click and enter button and then clears the textbox
 
 //will add description and time stamp
@@ -15,3 +9,9 @@ listenuserinput(enterbutton, doesthis)
 //needs to be a way to edit time stamp (for if you forget to add one)
 
 //needs to be a delete button for any added logs
+
+// This adds a list item if there's stuff typed and you click 'enter' 
+button.addEventListener("click", addAfterClick);
+
+// this adds a list item if there's something typed and you press 'enter' on keyboard. 
+input.addEventListener("keypress", addAfterKeypress);
